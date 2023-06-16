462ffe06dd895c6a39863acb0277335a9e23bebc
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-06-16 11:17:05 +0200

    bla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useRecoilState } from "recoil";
 import './App.css'
-import Login from './components/Login'
-import Users from './components/Users'
 import Channels from './components/Channels'
-import Message from './components/Messages'
-import AddUser from "./components/AddUser"
+import Login from './components/login'
 import { addNewUserState } from '../backend/data/recoil.js';
 
 
